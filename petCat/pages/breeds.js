0ed20eda32df0edc76b59d7deeb8461aed927903
Alexandr <yakovets.champ@gmail.com>
2023-08-27 21:33:22 +0300

    Navbar create

---
+++ petCat/pages/breeds.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+
+const Breeds = () => {
+    return (
+        <>
+            <Layout>
+            Breeds
+            </Layout>
+        </>
+    )
+};
+
+export default Breeds;
